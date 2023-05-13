f07b65fddbac33ec3330a2eba935e50db8fa9627
Amine-jabote <114097461+Amine-jabote@users.noreply.github.com>
2023-05-13 12:33:14 +0200

    Update Enemy.cs

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -1,10 +1,23 @@
+// Ce script permet de gérer les enemies.
+// Auteur: Jabote Mohamed Amine.
+// Date: 2023-04-01.
+// Dans le cadre de l'UE GL01 à l'UTT.
+// Semestre : P23.
+// Projet : Projet de jeu vidéo "Behe Goat".
+//langage: C#.
+
+//Appel des bibliothèques
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+//Début 
 public class Enemy : MonoBehaviour
 {
+
+    //Déclaration des variables
    public float speed;
    public int health; 
    public Sprite[] sprites;
@@ -23,40 +36,16 @@ public class Enemy : MonoBehaviour
 
    public Level1Manager manager;
 
+
+    //Initialisation
    void Awake() {
         spriteRender = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
         rigid.velocity = Vector2.left * speed;
    }
 
-   void Fire()
-   {
-    if (curShotDelay < maxShotDelay)
-        return;
-    if (enemyName == "S")
-    {
-        GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
-        Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
-        Vector3 dirVec = player.transform.position - transform.position;
-        rigid.AddForce(dirVec * 10, ForceMode2D.Impulse);
-    }
-    else if (enemyName == "L")
-    {
-        GameObject bulletR = Instantiate(bulletObjB, transform.position + Vector3.right * 0.3f, transform.rotation);
-        GameObject bulletL = Instantiate(bulletObjB, transform.position + Vector3.left * 0.3f, transform.rotation);
 
-        Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
-        Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
-
-        Vector3 dirVecR = player.transform.position - (transform.position + Vector3.right * 0.3f);
-        Vector3 dirVecL = player.transform.position - (transform.position + Vector3.left * 0.3f);
-
-        rigidR.AddForce(dirVecR.normalized * 10, ForceMode2D.Impulse);
-        rigidL.AddForce(dirVecL.normalized * 10, ForceMode2D.Impulse);
-    }
-
-    curShotDelay = 0;
-   }
+    //OnHit: Fonction qui permet de gérer les dégats infligés à l'ennemi
    public void OnHit(int dmg)
    {
     print("Hit!");
@@ -74,8 +63,11 @@ public class Enemy : MonoBehaviour
 
    void OnTriggerEnter2D(Collider2D collision)
    {
+    //Si l'objet "Bullet" entre en collision avec un objet "Enemy" alors on détruit l'objet "Bullet" et on inflige des dégats à l'objet "Enemy"
     if(collision.gameObject.CompareTag("BorderBullet"))
         Destroy(gameObject);
+
+    //Sinon si l'objet "PlayerBullet" entre en collision avec un objet "Enemy" alors on détruit l'objet "PlayerBullet" et on inflige des dégats à l'objet "Enemy"
     else if (collision.gameObject.CompareTag("PlayerBullet")){
         Bullet bullet = collision.gameObject.GetComponent<Bullet>();
         if (bullet != null) // Vérification si le GameObject possède bien le composant Bullet
@@ -84,9 +76,11 @@ public class Enemy : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
-    else if (collision.gameObject.CompareTag("Player"))
+    else if (collision.gameObject.CompareTag("Player"))//Sino si l'objet "Player" entre en collision avec un objet "Enemy" alors on détruit l'objet "Player"
     {
         collision.gameObject.SetActive(false);
    }
 }
+
+//Fin
 }
