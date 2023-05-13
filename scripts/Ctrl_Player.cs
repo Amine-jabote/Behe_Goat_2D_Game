c6c1ce4ec19edf2104545855d89b0601e0b7faa1
Amine-jabote <114097461+Amine-jabote@users.noreply.github.com>
2023-05-13 12:32:35 +0200

    Update Ctrl_Player.cs

--- scripts/Ctrl_Player.cs
+++ scripts/Ctrl_Player.cs
@@ -1,10 +1,24 @@
+// Ce script permet de gérer le personnage principale du jeu.
+// Auteur: Jabote Mohamed Amine.
+// Date: 2023-04-01.
+// Dans le cadre de l'UE GL01 à l'UTT.
+// Semestre : P23.
+// Projet : Projet de jeu vidéo "Behe Goat".
+//langage: C#.
+
+
+//Appel des bibliothèques
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+//Début
 public class Ctrl_Player : MonoBehaviour
 {
+
+    //Déclaration des variables
     public int life;
 
     public float speed;
@@ -25,19 +39,22 @@ public class Ctrl_Player : MonoBehaviour
 
     public Level1Manager manager;
 
+
     void Start()
     {
         manager = GameObject.Find("LevelManager").GetComponent<Level1Manager>();
     }
 
 
+    //Mis à jour
     void Update()
     {
+        //Si le joueur touche le haut de l'écran, il ne peut pas aller plus haut
         float h = Input.GetAxisRaw("Horizontal");
         if ((isTouchTop && h == 1) || (isTouchBottom && h == -1)){
             h = 0;
         }
-        
+        //Si le joueur touche le bas de l'écran, il ne peut pas aller plus bas
         float v = Input.GetAxisRaw("Vertical");
         if ((isTouchRight && v == -1) || (isTouchLeft && v == 1)){
             v = 0;
@@ -48,11 +65,12 @@ public class Ctrl_Player : MonoBehaviour
         transform.position = curPos + nextPos;
 
        
+       //Si le joueur touche le key "espace" dans son clavier le joueur tire
     if (Input.GetKeyDown(KeyCode.Space))
     {
         Fire();
     }
-
+    //Modidication du temps nécessaire pour tirer
     curShotDelay += Time.deltaTime;
 
         
@@ -61,18 +79,20 @@ public class Ctrl_Player : MonoBehaviour
     void Fire()
     {
         print("Fire");
-       /* if (!Input.GetButton(KeyCode.Space.ToString()))
-            return;*/
+        //Si le joueur n'a pas attendu le temps nécessaire pour tirer, il ne peut pas tirer
         if (curShotDelay < maxShotDelay)
             return;
+        //Si le joueur a attendu le temps nécessaire pour tirer, il peut tirer
         switch (power)
         {
+            //cas1: le joueur tire une balle
             case 1: 
             print("case 1");
                 GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
                 Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
                 rigid.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
                 break;
+            //cas 2: le joueur tire 3 balles
             case 2:
             print("case 2");
                 GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation);
@@ -91,6 +111,7 @@ public class Ctrl_Player : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
 {
+    //Si le joueur entre en collision avec un objet "Border", tout reste normal 
     if (collision.gameObject.tag == "Border")
     {
         switch (collision.gameObject.name)
@@ -109,15 +130,18 @@ public class Ctrl_Player : MonoBehaviour
                 break;
         }
     }
-    else if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet")
+    //Si le joueur entre en collision avec l'enemy il perd une vie
+    else if (collision.gameObject.tag == "Enemy")
     {
         life--;
         manager.UpdateLifeIcon(life);
+        //si le nombre de vies est inférieur ou égale à 0 => Game over
         if (life <= 0)
         {
             Destroy(collision.gameObject);
             manager.GameOver();
         }
+        //Sinon on respawn le joueur
         else
         {
             manager.RespawnPlayer();
@@ -150,7 +174,7 @@ void OnTriggerExit2D(Collider2D collision)
 
     }
 
-
+//Fin
 
 }
 
